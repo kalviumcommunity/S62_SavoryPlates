d127256459c5f44a1f4c6bac16540f42af18f98a
unknown <sera.shine@kalvium.community>
2025-01-08 16:17:00 +0530

    completed

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,17 @@
 if(process.env.NODE_ENV!=='PRODUCTION'){
     require('dotenv').config()
 }
+const mongoose=require('mongoose')
 const express = require('express');
 const connectDatabase = require('./db/database');
 const app =express();
 const PORT=process.env.PORT||3010
 app.get('/ping',(request,response)=>{
     return response.send('pong')
+})
+app.get('/',(req,res)=>{
+    const status=mongoose.connection.readyState===1?'Connected':'Not Connected';
+    res.send({status})
 })
 app.listen(PORT,()=>{
     connectDatabase()
