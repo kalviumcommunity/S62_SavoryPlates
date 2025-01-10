21ab4899a5c92466cffa7340160b0b13a9a46d3c
unknown <sera.shine@kalvium.community>
2025-01-10 11:36:01 +0530

    connected mongoclient

---
+++ Backend/db/mongo-client.js
@@ -0,0 +1,14 @@
+
+if(process.env.NODE_ENV !=='PRODUCTION'){
+    require('dotenv').config();
+}
+const mongoClient = require('mongodb').MongoClient;
+
+const connection = new mongoClient(process.env.DB_URL)
+async function getDB(){
+    let db = connection.db("S62_DumbDeals");
+    db=db.collection('Users');
+    return db;
+}
+
+module.exports = {getDB, connection};
