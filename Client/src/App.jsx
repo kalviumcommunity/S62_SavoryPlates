61d9defc19f77f81c06a1c2d662a8f6c9f1c15bc
unknown <sera.shine@kalvium.community>
2025-01-23 17:08:04 +0530

    design

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,7 @@
 import './App.css'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import SingleRecipePage from './components/SingleRecipePage';
 
 function App() {
   
@@ -12,6 +13,8 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
+        <Route path='/single' element={<SingleRecipePage/>}/>
+
       </Routes>
     </Router>
   )
